0a2994cc9a19c975a54676db91ff28f0193499e0
dangle-j <dangle@codesquad.kr>
2024-05-29 17:13:30 +0900

    LinkedList quiz

--- src/test/java/kr/codesquad/ListQuizTest.java
+++ src/test/java/kr/codesquad/ListQuizTest.java
@@ -3,6 +3,7 @@ package kr.codesquad;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -49,5 +50,21 @@ class ListQuizTest {
         // TODO 6 배열에서 1번 값을 삭제
 
         assertFalse(sampleList.contains('e'));
+    }
+
+    // LinkedList 에 대한 테스트
+    @Test
+    public void arrayList() throws Exception {
+        LinkedList<String> values = new LinkedList<>();
+        values.add("first");
+        values.add("second");
+        assertTrue(); // TODO 7 세 번째 값을 추가하라.
+        assertEquals(); // TODO 8 list의 크기를 구하라.
+        assertEquals(); // TODO 9 첫 번째 값을 찾아라.
+        assertTrue(); // TODO 10 "first" 값이 포함되어 있는지를 확인
+        assertEquals(); // TODO 11 첫 번째 값을 삭제해라.
+        assertEquals(); // TODO 12 11에서 삭제한 값이 삭제 됐는지 확인한다.
+
+        // TODO 13 values에 담긴 모든 값을 출력한다.
     }
 }
